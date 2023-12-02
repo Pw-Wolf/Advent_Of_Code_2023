3daacd536ba729bf0d921378ffedcb112e57c8f1
Pw-Wolf <klepic.sasa@gmail.com>
2023-12-02 13:25:52 +0100

    first day

---
+++ 1_day/main.py
@@ -0,0 +1,26 @@
+import re
+
+
+def get_input():
+	with open("./1_day/input", "r") as o:
+		data = o.read()
+	return data.split()
+
+def one(data):
+	return sum([ int(re.findall(r'\d', i)[0] + re.findall(r'\d', i)[-1]) for i in data])
+
+
+def two(data):
+	dict_nums = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9', '1':'1', '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9'}
+	rez = 0
+	for i in data:
+		nums = {ind:v for k, v in dict_nums.items() if (ind := i.find(k)) != -1}
+		nums.update({ind:v for k, v in dict_nums.items() if (ind := i.rfind(k)) != -1})
+		rez += int(nums[sorted(nums)[0]]+nums[sorted(nums)[-1]])
+	return rez
+
+
+if __name__ == "__main__":
+	data = get_input()
+	print(one(data))
+	print(two(data))
---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def get_key():
+	with open("session", "r") as o:
+		key = o.read()
+	return key
+
+def input_graber(url, key):
+
+	cookie = {
+		"session" : key
+	}
+	r = requests.get(url, cookies=cookie)
+	return r.text
+
+def store_text(dir, text):
+	with open(dir+"input", "w") as o:
+		o.write(text)
+
+if __name__ == "__main__":
+	day = "1"
+	url = f"https://adventofcode.com/2023/day/{day}/input"
+	dir = "./{day}_day/"
+	key = get_key()
+	text = input_graber(url, key)
+	store_text(dir, text)
